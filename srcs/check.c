5cff90fc9232f465ad61f4c803663c6e5f92b01f
Ilyass Bouiddou <ibouiddo@made-f0Ar5s3.clusters.42paris.fr>
2024-11-09 18:21:19 +0100

    09/11

--- srcs/check.c
+++ srcs/check.c
@@ -5,27 +5,39 @@ int check_int(char *str)
     int i;
     int j;
 
-    i = 1;
-    j = 0;
+    i = 0;
     while (str[i])
     {
-        if (str[j] == ft_isdigit)
-        {
-
-            j++;
-        }
+        if (ft_isdigit(str[i]) == 0)
+            return (1);
         i++;
     }
+    return (0);
+}
+int check_c
+int check_double(char *str)
+{
+    int i;
+
+    i = 0;
+
 }
 
 int check_all(int ac, char **av)
 {
     int i;
+    int j;
+    int nbr;
 
     i = 1;
+    j = 0;
+    nbr = 0;
     while (av[i])
     {
-        if1
+        nbr = ft_atoi(av[i]);
+        if (nbr == -2147483648 || nbr == 2148473647)
+            return (1);
+        if (check_int(nbr))
         i++;
     }
 }
